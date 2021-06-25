22969f364567817886873ac6630a687236f256a9
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-06-25 18:07:38 +0200

    feat(messages): insert new messages
    Improve messages component creating new form for insert new messages.

--- src/components/Congratulations/MessageForm.js
+++ src/components/Congratulations/MessageForm.js
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { HOST, APIPORT } from "../../tools/consts";
 
-function MessageForm() {
-  return <button>Deja tu mensaje</button>;
+function MessageForm(props) {
+  const [newMessage, setNewMessage] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+  const [validationErrors, setValidationErrors] = useState("");
+
+  const ERROR_LIST = {
+    API_ERROR: "El mensaje no se ha podido enviar.",
+    EMPTY_ERROR: "El mensaje/autor está vacío",
+  };
+
+  const handleChanges = (e) => {
+    if (e.target.id === "message") {
+      setNewMessage(e.target.value);
+    } else if (e.target.id === "author") {
+      setNewAuthor(e.target.value);
+    }
+  };
+
+  const sendMessage = () => {
+    if (validateMessage()) {
+      setValidationErrors("");
+
+      let msg = {
+        author: newAuthor,
+        message: newMessage,
+      };
+
+      let opts = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(msg),
+      };
+
+      const apiUrl = "/api/messages";
+      fetch(`${HOST}${APIPORT}${apiUrl}`, opts)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          if (data.success) {
+            props.fetchMessages();
+            setNewAuthor("");
+            setNewMessage("");
+          } else {
+            setValidationErrors(ERROR_LIST.API_ERROR);
+          }
+
+          //ENVIO DE CORREO A LOS NOVIOS!!
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+
+  const validateMessage = () => {
+    if (newMessage.length === 0 || newAuthor.length === 0) {
+      setValidationErrors(ERROR_LIST.EMPTY_ERROR);
+      return false;
+    }
+    //SANITIZAR
+    return true;
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <input
+          onChange={handleChanges}
+          type="text"
+          name="author"
+          id="author"
+          value={newAuthor}
+          placeholder="Autor"
+          maxLength="30"
+        ></input>
+        <input
+          onChange={handleChanges}
+          type="text"
+          name="message"
+          id="message"
+          value={newMessage}
+          placeholder="Tú mensaje"
+          maxLength="200"
+        ></input>
+        <button onClick={sendMessage} id="msg-button">
+          Enviar
+        </button>
+        <p>{validationErrors}</p>
+      </div>
+    </Fragment>
+  );
 }
 
 export default MessageForm;
--- src/components/Congratulations/index.js
+++ src/components/Congratulations/index.js
@@ -7,20 +7,30 @@ function Congratulations() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
+    fetchMessages();
+  }, []); //To avoid infity loop, set an empty array as second parameter.
+
+  const fetchMessages = () => {
     let allMessagesUrl = "/api/messages";
     fetch(`${HOST}${APIPORT}${allMessagesUrl}`)
       .then((response) => response.json())
       .then((data) => {
-        setMessages(data);
+        let randomSortedData = data.sort((a, b) => {
+          let randomA = Math.random();
+          let randomB = Math.random();
+          if (randomA >= randomB) return 1;
+          else return -1;
+        });
+        setMessages(randomSortedData);
       })
       .catch((error) => console.error(error));
-  }, []); //To avoid infity loop, set an empty array as second parameter.
+  };
 
   if (messages.length > 0) {
     return (
       <Fragment>
         <Messages messages={messages} />
-        <MessageForm />
+        <MessageForm fetchMessages={fetchMessages} />
       </Fragment>
     );
   } else {
