1ad5e0492d16c3d1131a9fe04bc79f53a2f4aa33
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-07-07 18:49:16 +0200

    fix(test): fix regeneratorRuntime error in jest
    Fix test error produced by async test.

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 // src/setupTests.js
 import { server } from "./src/test/mocks/server";
+
+//Regenator runtime to avoid jest error "regenerator runtime is not defined"
+//Stolen: https://stackoverflow.com/questions/42535270/regeneratorruntime-is-not-defined-when-running-jest-test
+import "regenerator-runtime/runtime";
+
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 
