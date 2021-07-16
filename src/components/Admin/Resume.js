16f1d80662e00c62c2c65a7e51b7c57198ae0a21
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-07-16 20:02:51 +0200

    feat(resume): create resume component

--- src/components/Admin/Resume.js
+++ src/components/Admin/Resume.js
@@ -1,9 +1,47 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import { HOST, APIPORT } from "../../tools/constants";
 
 export default function Resume() {
+  const [messages, setMessages] = useState([]);
+  const [guests, setGuests] = useState([]);
+
+  useEffect(() => {
+    fetchMessages();
+    fetchGuests();
+  }, []);
+
+  const fetchMessages = () => {
+    let urlMessages = "/api/messages";
+    fetch(`${HOST}${APIPORT}${urlMessages}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMessages(data);
+      });
+  };
+
+  const fetchGuests = () => {
+    let urlMessages = "/api/guests";
+    fetch(`${HOST}${APIPORT}${urlMessages}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setGuests(data);
+      });
+  };
+
+  let countMessages = messages.length;
+  let countGuests = guests.length;
+  let countBus = guests
+    .map((guest) => guest.bus)
+    .reduce((acc, next) => {
+      return next ? acc + next : acc;
+    }, 0);
+
   return (
     <Fragment>
       <h3>Resumen</h3>
+      <p>{countMessages} mensajes</p>
+      <p>{countGuests} invitados confirmados</p>
+      <p>{countBus} invitados con autobús</p>
     </Fragment>
   );
 }
--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -5,21 +5,22 @@ import Aside from "./Aside";
 import Resume from "./Resume";
 
 export default function Admin() {
-  const [section, setSection] = useState("guests");
+  const [section, setSection] = useState("resume");
 
   const changeSection = (e) => {
     let section = e.target.id.split("-")[1];
     setSection(section);
   };
 
-  let apartado = section === "resume" ? <Resume /> : <List section={section} />;
+  let currentSection =
+    section === "resume" ? <Resume /> : <List section={section} />;
 
   return (
     <Fragment>
       <h1>Admin</h1>
       <Logout />
       <Aside changeSection={changeSection} />
-      {apartado}
+      {currentSection}
     </Fragment>
   );
 }
