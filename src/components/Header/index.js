0ae89b5d11a1084bc1af73a6e9ea89b8c9c98465
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-07-18 17:25:50 +0200

    refactor(header): delete countdown from header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import CountDown from "./CountDown";
 
 function Header() {
   return (
     <header>
-      {/* <CountDown /> */}
-      <h3 className="wedding-date">26.02.22</h3>
+      <h3>26.02.22</h3>
       <h1>Bienvenidos a nuestra web</h1>
     </header>
   );
