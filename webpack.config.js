57a19cece75fa3a5c8e144355903f3cb19988810
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-09-07 19:36:47 +0200

    feat(deploy): change webpack fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: "source-map",
+  //devtool: "source-map",
   module: {
     rules: [
       {
@@ -40,17 +40,21 @@ module.exports = {
         ],
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-            },
-          },
-        ],
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
+      // {
+      //   test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "[name].[ext]",
+      //         outputPath: "fonts/",
+      //       },
+      //     },
+      //   ],
+      // },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
